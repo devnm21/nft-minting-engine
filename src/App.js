9f00fb336223bda6acc70b2568c4fbb03bcde2f9
Devakumar NM <pydev21@gmail.com>
2022-08-25 11:23:24 +0530

    feat: rem boilerpate UI and update with minting UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
+import MintNFTForm from "./components/MintNFTForm";
+import MintedNFT from "./components/MintedNFT";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+      }}
+    >
+      <Navbar />
+      <div className={"main-container"}>
+        <MintNFTForm />
+          <MintedNFT title={'#01'} />
+      </div>
     </div>
   );
 }
