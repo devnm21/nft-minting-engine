315bc0cb4b7ff39735570ad0a6e1dabc9fa80253
Devakumar NM <pydev21@gmail.com>
2022-08-25 11:22:21 +0530

    add MintedNFT

---
+++ src/components/MintedNFT/index.js
@@ -0,0 +1,18 @@
+import "./minterNFT.scss";
+const MintedNFT = (props) => {
+  return (
+    <div className={"form-container"}>
+      <img
+        src={
+          "https://cdn.myanimelist.net/r/360x360/images/characters/9/310307.jpg?s=56335bffa6f5da78c3824ba0dae14a26"
+        }
+      />
+        <div className={'nft-details'}>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </div>
+    </div>
+  );
+};
+
+export default MintedNFT;
